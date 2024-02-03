671ceac05c19fc39996a94157bbbb781e95e6ba5
Brandon <beegee54321@gmail.com>
2024-02-03 12:00:03 -0800

    adding folder/file structure and added user model code

---
+++ public/js/User.js
@@ -0,0 +1,53 @@
+class User extends Model {
+    checkPw() {
+        return true
+    }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+        },
+    },
+},
+{
+    hooks: {
+        beforeCreate: async () => {
+            //bcrypt/hash password and length code when set up
+            return;
+        },
+        beforeUpdate: async () => {
+            //bcrypt/hash password and length code when set up
+            return;
+        },
+    },
+    sequelize,
+    timestasmps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+}
+);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
