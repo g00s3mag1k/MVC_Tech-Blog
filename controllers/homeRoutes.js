1c87a713e57070ea4de1a69c2a10b72c5c57a4c9
Brandon <beegee54321@gmail.com>
2024-02-18 16:46:25 -0800

    adding to home routes and resolving functionality/linking

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,58 @@
-// const router = require('express').Router();
-// const { Project, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+const sequelize = require('../config/connection');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [{
+            model: User,
+            attributes: ['username'],
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        }
+    ],
+    })
+    .then(data => {
+        const posts = data.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
+            posts, loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
 
+router.get('/posts/:id', async (req, res) => {
+    try {
+       const postData = await Blogpost.findByPk(req.params.id, {
+        include: [{
+            model: User,
+            attributes: ['username'],
+        }],
+       });
+    const posts = postData.get({ plain: true });
+    
+       // Do something with postData
+    } catch (error) {
+       // Handle error
+    }
+});
 
-// module.exports = router;
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
