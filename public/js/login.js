fca51e9cd1fbb14dbe3f728d07f0264460d7b9d3
Brandon <beegee54321@gmail.com>
2024-02-22 21:06:53 -0800

    completed adding js will debug later

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,7 @@ const loginFormHandler = async (event) => {
   
     if (useranme && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+      const response = await fetch(`/api/users/login`, {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
