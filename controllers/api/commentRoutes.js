3837fb2ace5b269898994815ebb20f320f270c90
Brandon <beegee54321@gmail.com>
2024-04-04 01:22:25 -0700

    debugging/resolving functionality continued

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-//route to get all the comments
+
 router.get('/', (req, res) => {
     Comment.findAll({})
         .then(dbCommentData => res.json(dbCommentData))
@@ -12,7 +12,6 @@ router.get('/', (req, res) => {
         })
 });
 
-//route to get 1 comment
 router.get('/:id', (req, res) => {
     Comment.findAll({
             where: { 
@@ -26,14 +25,11 @@ router.get('/:id', (req, res) => {
 });
 
 
-//route to create a comment
 router.post('/', withAuth, (req, res) => {
-    // check session
     if (req.session) {
     Comment.create({
         comment_text: req.body.comment_text, 
         post_id: req.body.post_id,
-        // use the id from the session
         user_id: req.session.user_id,
     })
         .then(dbCommentData => res.json(dbCommentData))
@@ -44,7 +40,7 @@ router.post('/', withAuth, (req, res) => {
     }
 });
 
-//route to update a comment
+
 router.put('/:id', withAuth, (req, res) => {
     Comment.update({
         comment_text: req.body.comment_text
@@ -66,7 +62,6 @@ router.put('/:id', withAuth, (req, res) => {
 });
 
 
-//route to delete a comment
 router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
         where: {
